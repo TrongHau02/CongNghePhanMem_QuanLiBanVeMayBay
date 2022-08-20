3e247b5de6c7475a26bad727592c857990e341c9
TrongHau02 <107968646+TrongHau02@users.noreply.github.com>
2022-08-20 22:52:56 +0700

    file update

---
+++ QuanLiBanVeMayBay/QuanLiBanVeMayBay/DangNhap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLiBanVeMayBay
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult retThoat = MessageBox.Show("Bạn thực sự muốn thoát chương trình?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (retThoat == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            ConnectData dt = new ConnectData();
+            dt.Connect();
+            string sql = "Select * from NHANVIEN where manv = '" + txtTaiKhoan.Text 
+                + "' and matkhau = '" + txtMatKhau.Text + " ' ";
+            SqlCommand com = new SqlCommand(sql, dt.conn);
+            SqlDataReader reader = com.ExecuteReader();
+            if(reader.Read() == false)
+            {
+                MessageBox.Show("Đăng nhập không thành công !","Thông báo",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                txtTaiKhoan.Text = "";
+                txtMatKhau.Text = "";
+                txtTaiKhoan.Focus();
+            }
+            else
+            {
+                this.Hide();
+                frmMain main = new frmMain();
+                main.Show();
+            }
+            dt.Disconnect();
+        }
+    }
+}
---
+++ QuanLiBanVeMayBay/QuanLiBanVeMayBay/DoiMatKhau.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLiBanVeMayBay
+{
+    public partial class DoiMatKhau : Form
+    {
+        public DoiMatKhau()
+        {
+            InitializeComponent();
+        }
+
+        ConnectData dt = new ConnectData();
+
+        private void ntnQuayLai_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
